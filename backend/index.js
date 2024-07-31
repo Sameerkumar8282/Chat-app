36bb904bfe44f495edc3a66197bb4f20a4b722af
Sameer Kumar <sameerkumar1012.sk@gmail.com>
2024-07-31 16:38:09 +0530

    2nd fontend setup

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.get('/',(req,res)=>{
   res.send('Hello World!')
 })
 
+const PORT = process.env.PORT || 5000;
 app.listen(5000,()=>{
   console.log('server is running on port 5000');
 })
